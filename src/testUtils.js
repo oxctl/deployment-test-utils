7c06bcd1b64ef0d86b69385b20bb46093038070d
christopherjohnson <christophersebastianjohnson@gmail.com>
2026-09-23 11:05:16 +0100

    AB#133083 export grantAccess

--- src/testUtils.js
+++ src/testUtils.js
@@ -53,7 +53,7 @@ export const grantAccessIfNeeded = async (page, context, toolUrl) => {
  * @param {import('@playwright/test').FrameLocator} frameLocator - Locator for the LTI frame
  * @returns {Promise<void>}
  */
-const grantAccess = async (context, frameLocator) => {
+export const grantAccess = async (context, frameLocator) => {
   const button = await frameLocator.getByRole('button')
   const [newPage] = await Promise.all([
     context.waitForEvent('page'),
